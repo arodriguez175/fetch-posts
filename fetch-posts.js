45770951a3590af530e327cae957b760106df3c5
alex rodriguez <rodriguez.alexander223@gmail.com>
2023-02-21 22:52:43 -0500

    Added code for fetching, getting, and creating resources.

--- fetch-posts.js
+++ fetch-posts.js
@@ -1,3 +1,4 @@
+// Getting all resources
 // Making a GET request to the url endpoint
 // This url returns a list of posts in JSON format
 fetch('https://jsonplaceholder.typicode.com/posts')
@@ -9,3 +10,25 @@ which contains the response to the HTTP request */
 .then(data => console.log(data))
 // If there is an error during fetch, catch it and log it to the console
 .catch(error => console.log(error));
+
+// Getting a resource
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.log(error));
+
+// Creating a resource
+// Send a POST request to send request body to the url
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
