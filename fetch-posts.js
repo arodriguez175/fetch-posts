f91fd089ca83c4300e6ccdcf106e175b15b6f8fb
alex rodriguez <rodriguez.alexander223@gmail.com>
2023-02-22 09:45:18 -0500

    Added code to update a resource.

--- fetch-posts.js
+++ fetch-posts.js
@@ -32,3 +32,19 @@ fetch('https://jsonplaceholder.typicode.com/posts', {
 })
   .then((response) => response.json())
   .then((json) => console.log(json));
+
+  // Updating a resource
+  fetch('https://jsonplaceholder.typicode.com/posts/1', {
+    method: 'PUT',
+    body: JSON.stringify({
+      id: 1,
+      title: 'xyz',
+      body: 'abc',
+      userId: 1,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    }
+  })
+  .then(response => response.json())
+  .then(json => console.log(json));
